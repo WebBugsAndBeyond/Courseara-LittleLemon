7f8d70c5c97dd878a52ed79e2a0114c7a729d9ac
WebBugsAndBeyond <WebBugsAndBeyond@proton.me>
2025-08-10 14:03:15 -0400

    Adds new directory structure

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './WelcomeScreen';
-import SignupScreen from './SignupScreen';
+
+import RootNavigator from './navigators/RootNavigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Welcome'>
-            <Stack.Screen name='Welcome' component={WelcomeScreen} />
-            <Stack.Screen name='Signup' component={SignupScreen} />
-        </Stack.Navigator>
+        <RootNavigator />
     </NavigationContainer>
   );
 }
